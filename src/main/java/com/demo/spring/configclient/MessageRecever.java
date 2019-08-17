38f264f54e8bba89dd99565713bd9ae5a2602477
er.vinit@gmail.com <er.vinit@gmail.com>
2019-08-17 13:51:27 -0400

    First commit of congif-client

--- src/main/java/com/demo/spring/configclient/MessageRecever.java
+++ src/main/java/com/demo/spring/configclient/MessageRecever.java
@@ -2,10 +2,12 @@ package com.demo.spring.configclient;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("/rest")
 @RestController
 public class MessageRecever {
